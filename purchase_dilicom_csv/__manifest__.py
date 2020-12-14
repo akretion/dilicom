fdbc2e7b239c8e933751066b458ff86a072b0ca6
Alexis de Lattre <alexis.delattre@akretion.com>
2020-12-14 21:00:29 +0100

    [MIG] purchase_dilicom_csv to v14

--- purchase_dilicom_csv/__manifest__.py
+++ purchase_dilicom_csv/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
-# © 2014-2016 Akretion (http://www.akretion.com)
+# Copyright 2014-2020 Akretion France (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Purchase Dilicom CSV',
-    'version': '10.0.1.0.0',
+    'version': '14.0.1.0.0',
     'license': 'AGPL-3',
     'summary': "Generate CSV files to order on the Dilicom website",
     'description': """
@@ -23,7 +22,7 @@ This module has been written by Alexis de Lattre from Akretion
     """,
     'author': 'Akretion',
     'website': 'http://www.akretion.com',
-    'depends': ['purchase', 'report_qweb_txt'],
+    'depends': ['purchase'],
     'data': [
         'report/dilicom_purchase_order_csv.xml',
         'report.xml',
