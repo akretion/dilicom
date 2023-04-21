06ac0b4bf4ad3f7e44d2f2514c2cdcf109561d03
Alexis de Lattre <alexis.delattre@akretion.com>
2023-04-21 17:42:40 +0200

    [MIG] purchase_interforum_xlsx from v10 to v14

--- purchase_interforum_xlsx/__manifest__.py
+++ purchase_interforum_xlsx/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
-# Copyright 2018 Akretion (http://www.akretion.com)
+# Copyright 2018-2023 Akretion France (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Purchase Interforum XLSX',
-    'version': '10.0.1.0.0',
+    'version': '14.0.1.0.0',
     'license': 'AGPL-3',
     'summary': "Generate XLSX file for Interforum",
     'description': """
@@ -16,12 +15,12 @@ This module adds a report *Interforum XLSX* on purchase orders. It
 generates an XLSX file that can be uploaded on the Interforum website to
 generate an order.
 
-This module has been written by Alexis de Lattre from Akretion
+This module has been written by Alexis de Lattre from Akretion France
 <alexis.delattre@akretion.com>.
     """,
     'author': 'Akretion',
-    'website': 'http://www.akretion.com',
+    'website': 'https://github.com/akretion/dilicom',
     'depends': ['purchase', 'report_xlsx'],
     'data': ['report.xml'],
-    'installable': False,
+    'installable': True,
 }
--- purchase_interforum_xlsx/interforum_xlsx.py
+++ purchase_interforum_xlsx/interforum_xlsx.py
@@ -1,26 +1,25 @@
-# -*- coding: utf-8 -*-
-# Copyright 2018 Akretion France (http://www.akretion.com/)
+# Copyright 2018-2023 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.addons.report_xlsx.report.report_xlsx import ReportXlsx
+from odoo import models
 
 
-class InterforumXlsx(ReportXlsx):
+class InterforumXlsx(models.AbstractModel):
+    _name = "report.purchase.order.interforum.xlsx"
+    _inherit = "report.report_xlsx.abstract"
+    _description = "Interforum XLSX"
 
     def generate_xlsx_report(self, workbook, data, orders):
-        sheet = workbook.add_worksheet(u'Interforum')
+        sheet = workbook.add_worksheet('-'.join([o.name for o in orders]))
         regular = workbook.add_format({})
         sheet.set_column(0, 0, 20)
         i = 0
         for order in orders:
-            for l in order.order_line:
-                fallback = u'%s %s' % (l.product_id.display_name, 'NO BARCODE')
+            for line in order.order_line.filtered(lambda x: not x.display_type):
+                fallback = '%s %s' % (line.product_id.display_name, 'NO BARCODE')
                 sheet.write(
                     i, 0,
-                    l.product_id.barcode or fallback, regular)
-                sheet.write(i, 1, l.product_qty, regular)
+                    line.product_id.barcode or fallback, regular)
+                sheet.write(i, 1, line.product_qty, regular)
                 i += 1
-
-
-InterforumXlsx('report.purchase.order.interforum.xlsx', 'purchase.order')
